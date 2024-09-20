dd506944fa21085a193eb9af40f0f43ac27663cf
Trey Coury <rcoury@wisc.edu>
2024-09-20 14:27:52 -0500

    created getContributingPointsInfoForNitrateCells and scrDots

--- Central Sands Nitrate Correlation.R
+++ Central Sands Nitrate Correlation.R
@@ -1,13 +1,6 @@
 #Project Title: Central Sands Nitrate Correlation - Romano Database and Land Use
 #Project Description: evaluates the correlation between the nitrate levels from the romano database, and land use
 
-
-#TODO Next
-#I'm workign on getContributingPointsForNitrateCells and it's breaking my brain
-#I got a bit stuck with how to use R functions to essentially do some nested for loops for object lookup
-# that said, i think I should use a for loop to iterate over the vectors stored in nitrateCells$IntersectingFlowLines.
-# however, the last round of ChatGPT made sapply seem straightforward enough. I think the trick here is not to overthink. This problem has been solved before.
-
 #TODO
 #t01 - add ability to pull in contributing points
 #t02 - look at changing function outputs to just add to the same data frame, rather than making a bunch of data frames
@@ -32,6 +25,9 @@ library(dplyr)
 source("Central Sands Nitrate Estimator.R")
 
 # ----1 Define Functions----
+#' Read in shapefiles for our cells containing nitrate data
+#' Taken from the Romano Database
+#' @returns a dataframe of polygon shapefiles with Nitrate information
 getNitrateCells <- function() {
   nitrateCells <- st_read(dsn = "U:/Trey Coury/DS002-dataset01/CSGCC_Nitrate_Neonicotinoids.gdb", layer = "Nitrate_stats_per_section_Non_PWS_2017_2022")
   nitrateCells <- st_transform(nitrateCells, crs = 3070)
@@ -44,12 +40,63 @@ getFlowLinesInNitrateCell <- function(nitrateCells, allModpathFlowlines) {
   return(nitrateCells)
 }
 
-getContributingPointsForNitrateCells <- function(nitrateCells, timeFrameOfInterest, allModpathFlowlines) {
-  nitrateCells <- getFlowLinesInNitrateCell(nitrateCells, allModpathFlowlines)
-  #get contributing point IDs from flowlines t01
-  nitrateCells <- mutate()
+scrDots <- function(dotCount) {
+  if ((dotCount %% 1000) == 0 ) {
+    cat(dotCount %/% 1000)
+  }
+  else if ((dotCount %% 100) == 0) {
+    cat(".")
+  }
+  dotCount <- (dotCount + 1)
+  return(dotCount)
+}
 
+getContributingPointsInfoForNitrateCells <- function(nitrateCells, timeFrameOfInterest, allModpathFlowlines, allModpathStartingPoints) {
+  #Troubleshooting
+  nitrateCells <- head(nitrateCells, 320)
+  dotCount <- 0
+  #End troubleshooting
   
+  nitrateCells <- getFlowLinesInNitrateCell(nitrateCells, allModpathFlowlines)
+  
+  #ztrey left off here. I think the core of this is correct, so hooray!
+  #However, when i run the loop, the system errors when the which() statement searches for 153424. My guess is foating point?
+  #I see the rowIndex is stored as a long, so maybe convert stuff to ints?
+  
+  #get contributing point IDs from flowlines t01
+  #for each cell in our nitrate cell data frame
+  for (cell in 1:nrow(nitrateCells)) {
+    dotCount <- scrDots(dotCount)
+    flowlinesInCell <- nitrateCells[["IntersectingFlowlines"]][[cell]]
+    
+    #for each flowline in a cell
+    particleRowIndexVector <- c()
+    landUseVector <- c()
+    
+    for (flowlineIndex in flowlinesInCell) {
+      partidloc <- allModpathFlowlines[["conversion_to_partidloc_"]][[flowlineIndex]]
+      particleRowIndex <- which(allModpathStartingPoints$partidloc_ == partidloc)
+      landUse <- allModpathStartingPoints[["CDL_2022_2"]][[particleRowIndex]]
+      
+      #troubleshooting
+      #print(paste(cell, flowlineIndex, partidloc, particleRowIndex, landUse))
+      particleRowIndexVector <- c( particleRowIndexVector,particleRowIndex)
+      landUseVector <- c(landUseVector, landUse)
+    }
+    
+    if (length(particleRowIndexVector) == 0) {
+      particleRowIndexVector <- "no particles"
+    }
+    
+    if (length(landUseVector) == 0) {
+      landUseVector <- "no land use"
+    }
+    
+    nitrateCells[["ContributingParticleRowIndex"]][[cell]] <- particleRowIndexVector
+    nitrateCells[["ContributingParticleLandUse"]][[cell]] <- landUseVector
+  }
+
+  return(nitrateCells)  
 }
 
 # ----2 Main Callable Tag----
@@ -64,10 +111,11 @@ mainNitrateCorrelator <- function(){
   nitrateCells <- getNitrateCells()
   
   # ----2.3 Find contributing points for our Nitrate Cells----
-  nitrateCellsWithContributingPoints <- getContributingPointsForNitrateCells(nitrateCells,timeFrameOfInterest,allModpathFlowlines)
+  nitrateCellsWithContributingPointsInfo <- getContributingPointsInfoForNitrateCells(nitrateCells,timeFrameOfInterest,allModpathFlowlines,allModpathStartingPoints)
   
-    # ----2.4 Find the land use for the contributing points----
-  nitrateCellsWithLandUseMix <- getLandUseMix(allModpathStartingPoints, nitrateCellsWithContributingPoints, timeFrameOfInterest)
+  # ----2.4 Find the land use for the contributing points----
+  #ztrey - I think this is no longer needed
+  #nitrateCellsWithLandUseMix <- getLandUseMix(allModpathStartingPoints, nitrateCellsWithContributingPoints, timeFrameOfInterest)
   
   # ----2.5 Find the estimated nitrogen impacts given the land use----
   #this loads in a data frame of joined up data into a regression analysis tool
