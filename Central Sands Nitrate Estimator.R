43f03bb518ea4f8eed53c66ad3a27988a55b42cc
Trey Coury <rcoury@wisc.edu>
2024-11-15 09:55:00 -0600

    clean up comments; remove some code used for troubleshooting

--- Central Sands Nitrate Estimator.R
+++ Central Sands Nitrate Estimator.R
@@ -8,7 +8,6 @@
 #t08 - add handling for null results
 #t09 - confirm how I handle coordinate input and CRS syncing is correct
 #t10 - is using CRS 3070 okay?
-#t11 - re-do documentation for createPlots
 #t11.1 - review documentation
 #t13 - look at changing function outputs to just add to the same data frame, rather than making a bunch of data frames
 
@@ -36,25 +35,24 @@ library(dplyr)
 #' @returns an SF Point Object using CRS 3070
 createSFPoint <- function(xCoord, yCoord) {
   point_geom <- st_point(c(xCoord, yCoord))
-  sf_point <- st_sfc(point_geom, crs = 4326) #t00 create from long,lat coords, then switch to CRS 3070
+  sf_point <- st_sfc(point_geom, crs = 4326)
   sf_point <- st_transform(sf_point, crs = 3070)
   sf_point <- st_sf(geometry = sf_point)
   return(sf_point)
 }
 
-#' User input for the coordinates for which we'd like to estimate Nitrate levels.
-#' Coordinates are currently hard-coded, but could be modified to allow for user-input
+#' Gets the coordinates for which we'd like to estimate Nitrate levels.
+#' Coordinates are currently hard-coded, but could be modified to allow for front-end user input
 #' @returns coordinates stored as an SF Point Object
 getCoordsOfInterest <- function() {
-  #89.3551954°W 44.4980914°N
   xCoord <- -89.3551954
   yCoord <- 44.4980914
   coords <- createSFPoint(xCoord, yCoord)
   return(coords)
 }
 
-#' User input for the timeframe we should be looking back
-#' Time is currently hard-coded, but could be modified to allow for user-input
+#' Gets the time frame we should be looking back
+#' Time is currently hard-coded, but could be modified to allow for front-end user input
 #' @returns a time in years
 getTimeFrameOfInterest <- function() {
   timeInYears <- 100
@@ -62,7 +60,7 @@ getTimeFrameOfInterest <- function() {
 }
 
 #' User input for how big our buffer zone should be
-#' Buffer size is currently hard-coded, but could be modified to allow for user-input
+#' Buffer size is currently hard-coded, but could be modified to allow for front-end user input
 #' @returns a number for our buffer size
 getBuffer <- function() {
   buffer = 100 #t03, Ben's was set to 100, so just copying his for now.
@@ -71,19 +69,20 @@ getBuffer <- function() {
 
 #' Creates a buffer zone for a set of coordinates
 #' @param coordsOfInterest a set of coordinates for which to generate a buffer zone around
-#' @param buffer number, the size of the buffer to generate
+#' @param buffer the size of the buffer to generate
 #' @returns a buffer zone as a polygon object
 getCoordBufferZone <- function(coordsOfInterest, buffer) {
   coordBufferZone <- st_buffer(coordsOfInterest, dist = buffer)
   return(coordBufferZone)
 }
 
-#' Gets our modpath flowlines by reading them in from their shapefiles
+#' Gets our MODPATH flow lines by reading them in from their shape files
 #' Objects from this data set will b abbreviated as FLO objects
 #' source_file and conversion_to_partiloc_ are added so we can correctly match up IDs later on.
-#' (the fortran code used to generate the flowlines had to be run several times and could not directly create unique IDs, hence the conversions)
-#' (the specific numbers used have are a consequence of how the original ID genreation code was written)
-#' @returns a data frame that has our modpath flowlines, including a new column indicating which file a given row came from
+#' 
+#' Note: the fortran code used to generate the flow lines had to be run several times and could not directly create unique IDs, hence the need to subtract or add certain numbers
+#' the specific numbers used (-2 and +79740) come from how the original ID generation code was written.
+#' @returns a data frame that has our MODPATH flow lines, including a new column indicating which file a given row came from
 getFloDataSet <- function() {
   floDataSet0 <- st_read(dsn = "//ad.wisc.edu/wgnhs/Projects/Central_Sands_Nitrate_Transport/R_Analysis/Data Sets/Particles_updated_June2024/1particle_top_pathlines_0.shp")
   floDataSet0$source_file <- "pathlines0"
@@ -101,9 +100,9 @@ getFloDataSet <- function() {
   
   return(floDataSet)
 }
-#' Gets our modpath starting points by reading them from their shapefile
+#' Gets our MODPATH starting points by reading them from their shape file
 #' Objects from this data set will be abbreviated as STP objects
-#' @returns a data frame with our modpath starting points
+#' @returns a data frame with our MODPATH starting points
 getStpDataSet <- function() {
   stpDataSet <- st_read(dsn = "//ad.wisc.edu/wgnhs/Projects/Central_Sands_Nitrate_Transport/R_Analysis/Data Sets/Particles_updated_June2024/1particle_data_top_startpt.shp")
   return(stpDataSet)
@@ -111,8 +110,8 @@ getStpDataSet <- function() {
 
 #' Finds the flow lines that intersect with our buffer zone
 #' @param coordBufferZone a polygon object for the buffer zone in question 
-#' @param floDataSet linestring objects, flowlines from our modpath model output
-#' @returns a data frame of flowlines
+#' @param floDataSet linestring objects, flow lines from our MODPATH model output
+#' @returns a data frame of FLOs
 getFLOsInBufferZone <- function(coordBufferZone, floDataSet) {
   intersections <- st_intersects(coordBufferZone, floDataSet)
   flosInBufferZone <- floDataSet[intersections[[1]], ]
@@ -120,22 +119,22 @@ getFLOsInBufferZone <- function(coordBufferZone, floDataSet) {
 }
 
 #' Finds the contributing points (contribSTPs) of our flow lines
-#' @param flosInBufferZone a dataframe of linestring objects, the flowlines that intersected our buffered region
-#' @returns a dataframe of particle IDs for the contributing points. Note: the IDs are just a list of IDs, not the actual SF Objects.
+#' @param flosInBufferZone a data frame of linestring objects, the flow lines that intersected our buffered region
+#' @returns a data frame of particle IDs for the contributing points. Note: the IDs are just a list of IDs, not the actual SF Objects.
 #'          The actual SF objects need to be retrieved from the stpDataSet later on
 getContribSTPsFromFLOs <- function(flosInBufferZone) {
   contribSTPIDs <- data.frame(partidloc_ = flosInBufferZone$conversion_to_partidloc_)
   return(contribSTPIDs)
 }
 
-#' Get the starting points that contribute Nitrate to our area of interest
+#' Get the starting points that contribute to our area of interest
 #' @param coordsOfInterest a set of coordinates for which to generate a buffer zone around
-#' @param buffer number, the size of the buffer to generate
+#' @param buffer the size of the buffer to draw around our coordinates of interest
 #' @param timeFrameOfInterest a time in years
-#' @param floDataSet line string objects, flow lines from our modpath model output
+#' @param floDataSet line string objects, flow lines from our MODPATH model output
 #' @returns a list with the following structure:
 #'          stpIDs: a list of STP IDs for our contributing zones
-#'          floIDs: a list of FLO IDs for the flowlines that intersect the buffer zone of the selected area
+#'          floIDs: a list of FLO IDs for the flow lines that intersect the buffer zone of the selected area
 getContribSTPsForCoord <- function(coordsOfInterest, buffer, timeFrameOfInterest, floDataSet) {
   coordBufferZone <- getCoordBufferZone(coordsOfInterest, buffer)
   flosInBufferZone <- getFLOsInBufferZone(coordBufferZone,floDataSet)
@@ -146,8 +145,8 @@ getContribSTPsForCoord <- function(coordsOfInterest, buffer, timeFrameOfInterest
 
 #' Display the x,y coordinates for a given set of particles in the contributing zone
 #' Note: This function is not used in the main code, and is only intended to help with troubleshooting
-#' @param stpIDs a dataframe of particle IDs whose coordinates we want to display
-#' @param stpDataSet the list of starting points from our modpath model
+#' @param stpIDs a data frame of particle IDs whose coordinates we want to display
+#' @param stpDataSet a data frame of STPs from our MODPATH model
 displayCoordsForSTPIDs <- function(stpDataSet, stpIDs) {
   foreignKey <- c("partidloc_" = "partidloc_")
   stpIDs <- stpDataSet %>%
@@ -158,8 +157,8 @@ displayCoordsForSTPIDs <- function(stpDataSet, stpIDs) {
 
 
 #' Get the land use mix for our starting points
-#' @param stpIDs a dataframe of starting point particle IDs
-#' @param stpDataSet the list of starting points from our modpath model
+#' @param stpIDs a data frame of STP IDs
+#' @param stpDataSet a data frame of STPs from our MODPATH model
 #' @param timeFrameOfInterest a time in years
 #' @returns a data frame of land cover
 getLandCoverMix <- function(stpDataSet, stpIDs, timeFrameOfInterest) {
@@ -171,7 +170,7 @@ getLandCoverMix <- function(stpDataSet, stpIDs, timeFrameOfInterest) {
 }
 
 #' Summarize the contributing zones by landuse mix
-#' @param landCoverMix a dataframe with one row per particle IDs and a column for land cover
+#' @param landCoverMix a data frame with one row per STP ID and a column for land cover
 #' @returns a data frame with one row per land cover, a column for the Count of particle IDs with that land cover, and a column with the relative frequency of that land use
 getSummarizedLandCoverMix <- function(landCoverMix) {
   summarizedLandCoverMix <- landCoverMix %>%
@@ -183,12 +182,12 @@ getSummarizedLandCoverMix <- function(landCoverMix) {
   return(summarizedLandCoverMix)
 }
 
-#' Calculates the estimated nitrate levels given a landuse mix
-#' @param landCoverMix a dataframe with one row per particle IDs and a column for land cover
+#' Calculates the estimated nitrate levels given a land cover mix
+#' @param landCoverMix a data frame with one row per particle IDs and a column for land cover
 #' @return ???
 getEstimatedNitrateLevels <- function(landCoverMix) {
   # I think it'll eventually be an equation that does something like:
-  #      Nitrate = (c1)Corn + (c2)Potato + (c3)Woodlands, etc. where each 'c' is a contstant for how much land cover contributes to NO3.
+  #      Nitrate = (c1)Corn + (c2)Potato + (c3)Woodlands, etc. where each 'c' is a constant for how much land cover contributes to NO3.
   # We'd also like a way to do error bars. Which I *think* we'll just get for each term in our equation. And then...I think you just add the errors? I'll ask my Stats TAs
   # so i might need ways to hand:
   # -- a function to set up constants
@@ -200,8 +199,7 @@ getEstimatedNitrateLevels <- function(landCoverMix) {
   return(summarizedLandCovereMix)
 }
 
-#' Create some plots for our users
-#' @note: function documented on 9 September 2024 when it was like pre-alpha version. Update later as appropriate. t11
+#' Create a bar plot
 #' @param estimatedNitrateLevels a data frame of the landuse mix
 #' @returns a bar plot of nitrate data
 createPlots <- function(estimatedNitrateLevels) {
@@ -212,35 +210,34 @@ createPlots <- function(estimatedNitrateLevels) {
     labs(x = "Land Use", y = "Percent", title = barPlotTitle) +
     theme_minimal() +
     scale_y_continuous(labels = scales::percent)
-  ggsave("Relative Land Use of Contributing Zones.png", barPlot)
   return(barPlot)
 }
 
 #' Core logic for our nitrate estimator
 #' @param coordsOfInterest the coordinates for which to estimate nitrate levels
-#' @param timeFrameOfInterest the time frame over which we should look
+#' @param timeFrameOfInterest the time frame over which we should look (in years)
 #' @param buffer the radius of our buffer zone (in meters)
-#' @param floDataSet the flowlines generated from our MODPATH model
-#' @param stpDataSet the starting points from our MODPATH model
+#' @param floDataSet the flow lines (FLOs) generated from our MODPATH model
+#' @param stpDataSet the starting points (STPs) from our MODPATH model
 #' @returns a list with the following structure:
 #'            stpIDs: a data frame of contributing point IDs
 #'            floIDs: a data frame of FLO IDs that intersect with our selected buffer zone
 #'            landCover: Land Cover fraction of all of the contributing zones
 #'            Index X: Estimated nitrate level (Note: not yet added in)
 runNitrateEstimator <- function(coordsOfInterest, timeFrameOfInterest, buffer, floDataSet, stpDataSet) {
+  #Find contributing FLOs and STPs for the coordinates of interest
   contribSTPsForCoordReturnList <- getContribSTPsForCoord(coordsOfInterest, buffer, timeFrameOfInterest, floDataSet)
   contribSTPIDs <- contribSTPsForCoordReturnList$stpIDs
   contribFLOIDs <- contribSTPsForCoordReturnList$floIDs
  
-  # ----2.4 Find the land use for the contributing points----
+  # Find the land cover for the contributing points
   landCoverMix <- getLandCoverMix(stpDataSet, contribSTPIDs, timeFrameOfInterest)
   
-  # ----2.5 Find the estimated nitrogen impacts given the land use----
+  # Find the estimated nitrogen impacts given the land use
   estimatedNitrateLevels <- getEstimatedNitrateLevels(landCoverMix)
   
-  # ----2.6 Output to the user----
+  # Organize and return data
   nitrateEstimatorReturnList <- list(stpIDs = contribSTPIDs, floIDs = contribFLOIDs,landCover = estimatedNitrateLevels)
-  
   return(nitrateEstimatorReturnList)
 }
 
@@ -253,7 +250,7 @@ mainNitrateEstimator <- function() {
   timeFrameOfInterest <- getTimeFrameOfInterest()
   buffer <- getBuffer()
   
-  # ----2.2 Read in datafiles----
+  # ----2.2 Read in data----
   floDataSet <- getFloDataSet()
   stpDataSet <- getStpDataSet()
   
@@ -261,6 +258,5 @@ mainNitrateEstimator <- function() {
   nitrateEstimatorReturnList <- runNitrateEstimator(coordsOfInterest, timeFrameOfInterest, buffer, floDataSet, stpDataSet)
   plots <- createPlots(nitrateEstimatorReturnList$landCover)
   print(plots)
-  print("placeholder to know we're done")
 }
 
