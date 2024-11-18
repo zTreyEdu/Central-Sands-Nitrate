ea082954444f496336a72ddc17374397daf09adf
Trey Coury <rcoury@wisc.edu>
2024-11-18 14:36:45 -0600

    begin adding fluidrow styling

--- InteractiveMap/ui.R
+++ InteractiveMap/ui.R
@@ -5,17 +5,38 @@ dashboardPage(
   dashboardHeader(disable = TRUE),
   dashboardSidebar(disable = TRUE),
   dashboardBody(
-    box(width = 6,
-        p("Click or drag the marker within the bounded region to get estimated nitrate contribution from contributing zones."),
-        leafletOutput(outputId = "map")
-        ),
-    box(width = 6,
-        htmlOutput("coordInfo")
-        ),
-    box(width = 6,
-        plotOutput(outputId = "landCoverBarPlot")
-        )
+    fluidRow(
+      box(width = 6,
+          p("Click or drag the marker within the bounded region to get estimated nitrate contribution from contributing zones."),
+          leafletOutput(outputId = "map")
+          ),
+      box(width = 6,
+          plotOutput(outputId = "landCoverBarPlot")
+          )
+      ),
+      fluidRow(
+      box(width = 6,
+          htmlOutput("chartExplainer")
+          ),
+      box(width = 6,
+          htmlOutput("coordInfo")
+          )
+      ),
+      fluidRow(
+        column(4,
+      box(width = 6,
+          htmlOutput("externalLinks")
+          )),
+      column(4,
+      box(width = 6,
+          htmlOutput("takeAction")
+          )),
+      column(4,
+      box(width = 6,
+          htmlOutput("appExplainer")
+          ))
+      )
     ),
-  
-  title = "Interactive Nitrate Map"
+    
+  title = "Interactive Groundwater Flow Map"
 )
